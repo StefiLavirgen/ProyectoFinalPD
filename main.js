0f89f4b09723d23ddac7b00f050e7ebb3461bb5c
Stefania Rocio Lavirgen Ercoli <113640705+StefiLavirgen@users.noreply.github.com>
2022-12-15 17:43:19 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,89 @@
+function zoomIn()
+{
+    imgZoom1.style.height = "100%";
+    imgZoom1.style.width = "100%";
+}
+
+function zoomOut(){
+    imgZoom1.style.height = "";
+    imgZoom1.style.width = "";
+}
+
+function zoomIn2()
+{
+    imgZoom2.style.height = "100%";
+    imgZoom2.style.width = "100%";
+}
+
+function zoomOut2(){
+    imgZoom2.style.height = "";
+    imgZoom2.style.width = "";
+}
+
+function zoomIn3()
+{
+    imgZoom3.style.height = "100%";
+    imgZoom3.style.width = "100%";
+}
+
+function zoomOut3(){
+    imgZoom3.style.height = "";
+    imgZoom3.style.width = "";
+}
+
+function zoomIn4()
+{
+    imgZoom4.style.height = "100%";
+    imgZoom4.style.width = "100%";
+}
+
+function zoomOut4(){
+    imgZoom4.style.height = "";
+    imgZoom4.style.width = "";
+}
+
+/*abajo*/
+
+function zoomInAbajo5()
+{
+    imgZoom5.style.height = "100%";
+    imgZoom5.style.width = "100%";
+}
+
+function zoomOutAbajo5(){
+    imgZoom5.style.height = "";
+    imgZoom5.style.width = "";
+}
+
+function zoomInAbajo6()
+{
+    imgZoom6.style.height = "100%";
+    imgZoom6.style.width = "100%";
+}
+
+function zoomOutAbajo6(){
+    imgZoom6.style.height = "";
+    imgZoom6.style.width = "";
+}
+
+function zoomInAbajo7()
+{
+    imgZoom7.style.height = "100%";
+    imgZoom7.style.width = "100%";
+}
+
+function zoomOutAbajo7(){
+    imgZoom7.style.height = "";
+    imgZoom7.style.width = "";
+}
+
+function zoomInAbajo8()
+{
+    imgZoom8.style.height = "100%";
+    imgZoom8.style.width = "100%";
+}
+
+function zoomOutAbajo8(){
+    imgZoom8.style.height = "";
+    imgZoom8.style.width = "";
+}
